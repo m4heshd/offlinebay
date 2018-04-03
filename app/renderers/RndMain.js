7dada4ffbf14dd104eeb294094b7521065a50508
TechTac <techtacyt@gmail.com>
2018-04-03 21:29:41 +0530

    Notification system and Renderer jQuery transformation

--- app/renderers/RndMain.js
+++ app/renderers/RndMain.js
@@ -4,23 +4,52 @@ const {ipcRenderer} = electron;
 /* Window controls
 --------------------*/
 ipcRenderer.on('maxed', function () {
-    let i = document.querySelector('#btnMaximize i');
-    i.classList.remove('zmdi-window-maximize');
-    i.classList.add('zmdi-window-restore');
+    $('#btnMaximize i').removeClass('zmdi-window-maximize').addClass('zmdi-window-restore');
 });
 ipcRenderer.on('restored', function () {
-    let i = document.querySelector('#btnMaximize i');
-    i.classList.remove('zmdi-window-restore');
-    i.classList.add('zmdi-window-maximize');
+    $('#btnMaximize i').removeClass('zmdi-window-restore').addClass('zmdi-window-maximize');
 });
 
-document.querySelector('#btnClose').addEventListener('click', function () {
+$('#btnClose').on('click', function () {
     ipcRenderer.send('app-close');
-} );
-document.querySelector('#btnMinimize').addEventListener('click', function () {
+});
+$('#btnMinimize').on('click', function () {
     ipcRenderer.send('app-min');
-} );
-document.querySelector('#btnMaximize').addEventListener('click', function () {
+});
+$('#btnMaximize').on('click', function () {
     ipcRenderer.send('app-max');
-} );
+});
+
 /*-----------------------------*/
+function popMsg(txt, type) {
+    return function () {
+        $.notify({
+            message: txt
+        }, {
+            type: type,
+            placement: {
+                from: 'bottom',
+                align: 'right'
+            },
+            animate: {
+                enter: 'animated fadeInUp',
+                exit: 'animated fadeOutDown'
+            },
+            template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert">\n' +
+            '    <button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button>\n' +
+            '    <span data-notify="icon"></span> \n' +
+            '    <span data-notify="title">{1}</span> \n' +
+            '    <span data-notify="message" style="vertical-align: middle">{2}</span>\n' +
+            '    <div class="progress" data-notify="progressbar">\n' +
+            '        <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '    <a href="{3}" target="{4}" data-notify="url"></a>\n' +
+            '</div>'
+        });
+    }
+}
+
+$(document).ready(function () {
+    $('#btnOpenMag').on('click', popMsg('Magnet link opened in the default application', 'info'));
+});
